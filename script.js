61ed1ff169845249f937b10467db36eb98975cb5
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-02 00:02:12 -0500

    created select element

---
+++ script.js
@@ -0,0 +1,8 @@
+import Select from './select'
+
+const selecElements = document.querySelectorAll('[data-custom]')
+
+//if more select elements are added in future we would loop over each 
+selectElements.forEach(selectElements => {
+    new Select(selectElement)
+})
---
+++ select.js
@@ -0,0 +1,7 @@
+// all select code goes here 
+export default class Select {
+    //this constructor takes the slectElement from index.html
+    constructor(element) {
+    this.element = element
+    }
+} 
